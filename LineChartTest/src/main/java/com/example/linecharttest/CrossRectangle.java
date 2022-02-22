19f6b20c60c3be7c04a1acce494b1252e92aa53f
yasser <yasserboulahsen414@gmail.com>
2022-02-22 14:06:50 -0500

    todo added for test

--- LineChartTest/src/main/java/com/example/linecharttest/CrossRectangle.java
+++ LineChartTest/src/main/java/com/example/linecharttest/CrossRectangle.java
@@ -4,11 +4,13 @@ import javafx.scene.shape.Rectangle;
 
 public class CrossRectangle extends Rectangle {
 
-
+//@TODO add line to rectangle
     public CrossRectangle() {
         this.setHeight(20);
         this.setWidth(20);
     }
+
+
 
 
 
