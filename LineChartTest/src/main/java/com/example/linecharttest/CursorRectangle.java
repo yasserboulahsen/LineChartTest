36a5d4bf25f63eab088f82cfe9972993507a7d3a
yasserboulahsen <yasserboulahsen@gmail.com>
2022-07-05 15:18:37 -0400

    delete icon fade style added

--- LineChartTest/src/main/java/com/example/linecharttest/CursorRectangle.java
+++ LineChartTest/src/main/java/com/example/linecharttest/CursorRectangle.java
@@ -1,5 +1,6 @@
 package com.example.linecharttest;
 
+import javafx.animation.FadeTransition;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -12,6 +13,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -31,13 +33,12 @@ public class CursorRectangle extends Group {
     private Group group;
 
     private Label label;
-    private  Label deleteCrossLabel;
+    private Label deleteCrossLabel;
     double initialSceneX, initialSceneY;
     double initialTranslateX, initialTranslateY;
 
     private Node node;
     private XYChart.Series<?, ?> series;
-
 
 
     public CursorRectangle(Node node, XYChart.Series<?, ?> series) {
@@ -54,15 +55,15 @@ public class CursorRectangle extends Group {
         this.verticalLineTop.setStyle("-fx-stroke-dash-array: 2;");
         this.verticalLineDown.setStyle("-fx-stroke-dash-array: 2;");
         this.label = new Label();
-        this.deleteCrossLabel= new Label();
+        this.deleteCrossLabel = new Label();
         this.setOnMouseDragged(rectangleOnMouseDraggedEventHandler);
         this.setOnMousePressed(rectangleOnMousePressedEventHandler);
         this.setOnMouseClicked(rectangleOnMouseClickedEventHandler);
         this.deleteCrossLabel.setOnMouseClicked(deleteLabelOnMouseClickedEvent);
         this.deleteCrossLabel.setOnMouseEntered(hoverOverLabel);
-        this.deleteCrossLabel.setOnMouseExited((event)-> deleteCrossLabel.setStyle("-fx-background-color:rgba(0, 0, 0, 0)"));
+        this.deleteCrossLabel.setOnMouseExited((event) -> deleteCrossLabel.setStyle("-fx-background-color:rgba(0, 0, 0, 0)"));
         getRectangleWithLines();
-        this.getChildren().addAll(this.verticalLineTop, this.verticalLineDown, this.rectangleCross, this.horizontalLineRight, this.horizontalLineLeft, this.label,this.deleteCrossLabel);
+        this.getChildren().addAll(this.verticalLineTop, this.verticalLineDown, this.rectangleCross, this.horizontalLineRight, this.horizontalLineLeft, this.label, this.deleteCrossLabel);
         this.label.setId("labelChart");
 
 
@@ -102,6 +103,7 @@ public class CursorRectangle extends Group {
     public RectangleCross getRectangleCross() {
         return rectangleCross;
     }
+
     public CursorRectangle getCursorRectangle() {
         return this;
     }
@@ -119,7 +121,6 @@ public class CursorRectangle extends Group {
                     initialSceneY = t.getSceneY();
                     initialTranslateX = ((Group) (t.getSource())).getTranslateX();
                     initialTranslateY = ((Group) (t.getSource())).getTranslateY();
-
 
 
                     //label.setText("");
@@ -154,49 +155,59 @@ public class CursorRectangle extends Group {
                     verticalLineDown.setEndY(verticalLineDown.getBoundsInParent().getMaxY() + y2);
                     //
 
-                    double xMax =  getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterX() +10;
-                    double xMin =  getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterX() -10;
-                    double yMax =  getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterY() +10;
-                    double yMin =  getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterY() -10;
+                    double xMax = getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterX() + 10;
+                    double xMin = getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterX() - 10;
+                    double yMax = getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterY() + 10;
+                    double yMin = getCursorRectangle().localToParent(node.getBoundsInLocal()).getCenterY() - 10;
 
 
+                    for (XYChart.Data<?, ?> data : getSeries().getData()) {
+                        //data.getNode().toFront();
 
-                    for(XYChart.Data<?, ?> data : getSeries().getData()){
-                            //data.getNode().toFront();
+                        if ((xMin < data.getNode().getBoundsInParent().getCenterX() && (data.getNode().getBoundsInParent().getCenterX() < xMax) &&
+                                (yMin < data.getNode().getBoundsInParent().getCenterY() && data.getNode().getBoundsInParent().getCenterY() < yMax))) {
 
-                            if ((xMin <  data.getNode().getBoundsInParent().getCenterX() && ( data.getNode().getBoundsInParent().getCenterX() < xMax) &&
-                                    (yMin <  data.getNode().getBoundsInParent().getCenterY() &&  data.getNode().getBoundsInParent().getCenterY() < yMax) )) {
+                            label.setText("x: " + data.getXValue() + "\ny: " + data.getYValue());
+                            label.getStyleClass().add("labelChart");
+                            getRectangleCross().setStrokeWidth(4);
+                            System.out.println(getCursorRectangle().getBoundsInLocal().getCenterX());
+                            System.out.println(getCursorRectangle().getBoundsInLocal().getCenterY());
 
-                                label.setText("x: "+data.getXValue() + "\ny: " + data.getYValue());
-                                label.getStyleClass().add("labelChart");
-                                getRectangleCross().setStrokeWidth(4);
 //                               data.getNode().setStyle("-fx-background-color: black, white;\n"
 //                    + "    -fx-background-insets: 0, 5;\n"
 //                    + "    -fx-background-radius: 5px;\n"
 //                    + "    -fx-padding: 5px;");
 
-                               break;
-                            } else {
-                                label.setText("");
-                                label.getStyleClass().clear();
-                                getRectangleCross().setStrokeWidth(1.5);
+                            break;
+                        } else {
+                            label.setText("");
+                            label.getStyleClass().clear();
+                            getRectangleCross().setStrokeWidth(1.5);
 
 
-                            }
+
+                        }
 
 
-                    };
+                    }
+                    ;
 
                 }
             };
-    EventHandler<MouseEvent>  rectangleOnMouseClickedEventHandler = new EventHandler<MouseEvent>() {
+    EventHandler<MouseEvent> rectangleOnMouseClickedEventHandler = new EventHandler<>() {
         @Override
         public void handle(MouseEvent mouseEvent) {
-                     Image image = new Image("file:src/main/resources/pics/delPic.png",25,25,false,false);
-                        if(mouseEvent.getButton() == MouseButton.SECONDARY){
-                            deleteCrossLabel.setVisible(true);
-                            deleteCrossLabel.setGraphic(new ImageView(image));
-                        }
+            Image image = new Image("file:src/main/resources/pics/delPic.png", 25, 25, false, false);
+
+            if (mouseEvent.getButton() == MouseButton.SECONDARY) {
+
+                deleteCrossLabel.setVisible(true);
+                deleteCrossLabel.setGraphic(new ImageView(image));
+                FadeTransition fadeDelete = new FadeTransition(Duration.millis(1000), deleteCrossLabel);
+                fadeDelete.setFromValue(0.3);
+                fadeDelete.setToValue(1);
+                fadeDelete.play();
+            }
         }
     };
     EventHandler<MouseEvent> deleteLabelOnMouseClickedEvent = mouseEvent -> {
@@ -207,16 +218,14 @@ public class CursorRectangle extends Group {
     };
 
 
-    public void deleteCursor(BorderPane borderPane){
+    public void deleteCursor(BorderPane borderPane) {
         borderPane.setOnKeyPressed(keyEvent -> {
             if (Objects.equals(keyEvent.getCode().toString(), "DELETE")) {
                 getCursorRectangle().deleteCursor(borderPane);
-               // System.out.println(this.lookup("#rectangle"));
+                // System.out.println(this.lookup("#rectangle"));
             }
         });
     }
-
-
 
 
 }
